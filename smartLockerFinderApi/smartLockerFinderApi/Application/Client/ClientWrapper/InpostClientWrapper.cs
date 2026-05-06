4590eb4b9239fef8d38dc890c11c62c250a166e5
michal <michu.romanczuk@gmail.com>
2026-05-06 22:46:46 +0200

    Handler refactor -added InpostClientWrapper

---
+++ smartLockerFinderApi/smartLockerFinderApi/Application/Client/ClientWrapper/InpostClientWrapper.cs
@@ -0,0 +1,26 @@
+﻿using Inwentaryzator_paczkomatow_Api.Domain.Entities;
+using MediatR;
+using smartLockerFinderApi.Application.Dto;
+using smartLockerFinderApi.Domain.Intefaces;
+
+namespace smartLockerFinderApi.Application.Client.ClientWrapper;
+
+public class InpostClientWrapper
+{
+    private readonly HttpClient _httpClient;
+    private readonly IParcelLockerService _parcelLockerService;
+
+    InpostClientWrapper(HttpClient httpClient, IParcelLockerService parcelLockerService)
+    {
+        _httpClient = httpClient;
+        _parcelLockerService = parcelLockerService;
+    }
+
+    public async Task<ParcelLockerApiResponse> GetParcelLockerData(LocationData location, CancellationToken cancellationToken)
+    {
+        var url = _parcelLockerService.BuildPointsUrl(location);
+        var responseData = await _httpClient.GetFromJsonAsync<ParcelLockerApiResponse>(url, cancellationToken);
+
+        return responseData;
+    }
+}
---
+++ smartLockerFinderApi/smartLockerFinderApi/Application/Client/Interface/IInpostClientWrapper.cs
@@ -0,0 +1,10 @@
+﻿using Inwentaryzator_paczkomatow_Api.Domain.Entities;
+using smartLockerFinderApi.Application.Dto;
+
+namespace smartLockerFinderApi.Application.Client.Interface
+{
+    public interface IInpostClientWrapper
+    {
+        Task<ParcelLockerApiResponse> GetParcelLockerData(LocationData location, CancellationToken cancellationToken);
+    }
+}
